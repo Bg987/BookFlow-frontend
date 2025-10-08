671f710d9e3ebba4580be88026f4d39faa5cee78
Bg <119032764+Bg987@users.noreply.github.com>
2025-10-08 22:56:13 +0530

    url change fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://bookflow-1ceq.onrender.com//api"; // backend URL
+const BASE_URL = "https://bookflow-1ceq.onrender.com/api"; // backend URL
 //http://localhost:5000api
 const api = axios.create({
   baseURL: BASE_URL,
